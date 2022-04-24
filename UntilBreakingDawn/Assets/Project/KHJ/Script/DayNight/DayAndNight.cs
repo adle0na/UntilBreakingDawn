d78705851d5f5f26becfef47ba325447149b5353
mental <khz0825@naver.com>
2022-04-24 12:54:11 +0900

    2022_04_24 낮밤 실제시간 적용
    낮 밤 적용은 가능하나 낮밤의 자연스런 변경을 원하면 수정할 예정

--- UntilBreakingDawn/Assets/Project/KHJ/Script/DayNight/DayAndNight.cs
+++ UntilBreakingDawn/Assets/Project/KHJ/Script/DayNight/DayAndNight.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class DayAndNight : MonoBehaviour
 {
-    [SerializeField] private float ChangeDayCount = 1.0f;   // 낮 밤 바꾸기 시작 시간초 
     [SerializeField] private float dayChange = 10.0f;       // 낮 밤 바꾸기 최대 시간초
-    [SerializeField] private float RotateDay = 0.1f;        // 게임 100초 = 현실 1초
+
+    private float ChangeDayCount = 1.0f;   // 낮 밤 바꾸기 시작 시간초 
+    private float RotateDay = 180f;        // 180도로 바꿔서 낮밤 유지
 
     private SceneManager Scene;             // 씬 바꾸기
 
@@ -16,11 +17,13 @@ public class DayAndNight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 게임 100초 = 현실 시간 1초
+        // 지금은 현실 시간으로 계산
         ChangeDayCount += Time.deltaTime;
-
+        Debug.Log(ChangeDayCount);
         if (ChangeDayCount >= dayChange)
         {
-            transform.Rotate(Vector3.right, RotateDay * Time.deltaTime);
+            transform.Rotate(Vector3.right, RotateDay);
 
             if (!isNight)
             {
